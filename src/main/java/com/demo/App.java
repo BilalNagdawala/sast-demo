030086f722b25eeffc467edf491989b939a32e9c
BilalNagdawala <bilalnagdawala@gmail.com>
2026-09-23 12:23:25 +0530

    Initial SAST demo project

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("SAST Demo Application");
+
+        String username = "student";
+
+        if (username != null) {
+            System.out.println("Welcome " + username);
+        }
+    }
+}
